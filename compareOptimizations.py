b9fbdf694a5e6d5f39f0ef980173543433dd8e59
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2020-11-19 00:06:01 -0600

    moved entire contents of output string to one line

--- compareOptimizations.py
+++ compareOptimizations.py
@@ -10,5 +10,4 @@ os.system("python3 baragiola_optimization_generalization_rmsd_generalization.py"
 endTime2 = int(round(time.time() * 1000))
 
 print("Original (baragiola_optimization.py) took " + str(endTime1 - startTime1) + " milliseconds")
-print("Generalization (baragiola_optimization_generalization_rmsd_generalization.py) took " + str(endTime2 - startTime2) + " 
-milliseconds")
+print("Generalization (baragiola_optimization_generalization_rmsd_generalization.py) took " + str(endTime2 - startTime2) + " milliseconds")
