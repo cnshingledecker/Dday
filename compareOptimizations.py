64909ed6ad687254b6ceec76841c7c91db78d3f5
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2020-11-21 23:51:27 -0600

    Added code that compares the photo_processes and results files produced by the original and generalized optimizations to see if the optimizations produced the same output

--- compareOptimizations.py
+++ compareOptimizations.py
@@ -1,5 +1,6 @@
 import time
 import os
+from exportable_custom_functions import is_float, is_int
 
 # startTime1 = int(round(time.time() * 1000))
 # os.system("python3 baragiola_optimization.py")
@@ -17,3 +18,97 @@ endTime3 = int(round(time.time() * 1000))
 # print("Original (baragiola_optimization.py) took " + str((endTime1 - startTime1) / 1000) + " seconds")
 print("Generalization (baragiola_optimization_generalization_rmsd_generalization.py) took " + str((endTime2 - startTime2) / 1000) + " seconds")
 print("Modified original (baragiola_optimization_test.py) took " + str((endTime3 - startTime3) / 1000) + " seconds")
+
+
+# Compare the results from the original optimization and the generalized optimization
+num_errors = 0
+
+# Compare the output in the results files
+results_original = open('results_3', 'r')
+results_gen_rmsd_gen = open('results_2', 'r')
+results_original_lines = results_original.readlines()
+results_gen_rmsd_gen_lines = results_gen_rmsd_gen.readlines()
+
+if(len(results_original_lines) == len(results_gen_rmsd_gen_lines)):
+    for i in range (0, len(results_original_lines)): # It doesn't matter which list length we use, because if we made it inside this if statement, the lengths are the same
+        if(len(results_original_lines[i].strip()) > 0 and len(results_gen_rmsd_gen_lines[i].strip()) > 0): # Skips over lines that look empty in the files, but are really whitespace (the line consists of a positive number of whitespaces)
+            results_original_lines_val = results_original_lines[i][0:23].rstrip()
+            results_gen_rmsd_gen_lines_val = results_original_lines[i][0:23].rstrip()
+            if(results_original_lines_val != results_gen_rmsd_gen_lines_val):
+                num_errors += 1
+                print("Error: the values on line " + str(i) + " do not match. They are " + results_original_lines_val + " and " + results_gen_rmsd_gen_lines_val)  
+else:
+    num_errors += 1
+    print("Error: A different number of fitting factor combinations were processed.")
+
+results_original.close()
+results_gen_rmsd_gen.close()
+
+
+# Compare the results in the lines of the photo_processes files produced
+photo_processes_original = open('photo_processes_3.dat', 'r')
+photo_processes_gen_rmsd_gen = open('photo_processes_2.dat', 'r')
+photo_processes_original_lines = photo_processes_original.readlines()
+photo_processes_gen_rmsd_gen_lines = photo_processes_gen_rmsd_gen.readlines()
+
+line_contains_text_photo_processes_original = []
+line_contains_text_photo_processes_gen_rmsd_gen = []
+
+num_non_empty_lines_photo_processes_original = 0
+for i in range(0, len(photo_processes_original_lines)):
+    if(len(photo_processes_original_lines[i].strip()) > 1):
+        num_non_empty_lines_photo_processes_original += 1
+        line_contains_text_photo_processes_original.append(True)
+    else:
+        print(str(i + 1) + " Original")
+        line_contains_text_photo_processes_original.append(False)
+
+num_non_empty_lines_photo_processes_gen_rmsd_gen = 0
+for i in range(0, len(photo_processes_gen_rmsd_gen_lines)):
+    if(len(photo_processes_gen_rmsd_gen_lines[i].strip()) > 1):
+        num_non_empty_lines_photo_processes_gen_rmsd_gen += 1
+        line_contains_text_photo_processes_gen_rmsd_gen.append(True)
+    else:
+        print(str(i + 1) + " Gen")
+        line_contains_text_photo_processes_gen_rmsd_gen.append(False)
+
+if(num_non_empty_lines_photo_processes_gen_rmsd_gen == num_non_empty_lines_photo_processes_original):
+    index_line_to_compare_original = 0
+    index_line_to_compare_gen_rmsd_gen = 0
+
+    for i in range(0, num_non_empty_lines_photo_processes_original):    # It doesn't matter which list length we use, because if we made it inside this if statement, the lengths are the same
+        # The below for loops ensure that the correct (non-empty) line from each photo_processes file is being compared
+        while(line_contains_text_photo_processes_original[index_line_to_compare_original] == False): 
+            index_line_to_compare_original += 1
+        while(line_contains_text_photo_processes_gen_rmsd_gen[index_line_to_compare_gen_rmsd_gen] == False):
+            index_line_to_compare_gen_rmsd_gen += 1
+        
+        original_line_as_list = photo_processes_original_lines[index_line_to_compare_original].split()
+        gen_line_as_list = photo_processes_gen_rmsd_gen_lines[index_line_to_compare_gen_rmsd_gen].split()
+
+        if(line_contains_text_photo_processes_original[i] and line_contains_text_photo_processes_gen_rmsd_gen[i]):
+            # We need to find the first index in the original line (as a list) where there is a fitting factor value (it is the first number besides the first element of the list, hence why i (below) is set to 1)
+            index_first_float_value_in_original_line = 1  # The first entry in the line is a float, but we are looking for the first fitting factor (the 2nd float value in the line); starting at 1 therefore makes the implementation of the below while loop easier
+            # The while loop below skips the first value because it is an integer and would thus be counted as a float. 
+            while(index_first_float_value_in_original_line < len(original_line_as_list) and not(is_float(original_line_as_list[index_first_float_value_in_original_line]))):  # Increments the index value until the first float value (fitting factor value) is found
+                index_first_float_value_in_original_line += 1
+
+            # We need to find the first index in the generalization line (as a list) where there is a fitting factor value (it is the first number besides the first element of the list, hence why i (below) is set to 1)
+            index_first_float_value_in_gen_line = 1  # The first entry in the line is a float, but we are looking for the first fitting factor (the 2nd float value in the line); starting at 1 therefore makes the implementation of the below while loop easier
+            # The while loop below skips the first value because it is an integer and would thus be counted as a float. 
+            while(index_first_float_value_in_gen_line < len(gen_line_as_list) and not(is_float(gen_line_as_list[index_first_float_value_in_gen_line]))):  # Increments the index value until the first float value (fitting factor value) is found
+                index_first_float_value_in_gen_line += 1
+
+            for j in range(0, 3):
+                if(float(original_line_as_list[index_first_float_value_in_original_line + j]) != float(gen_line_as_list[index_first_float_value_in_gen_line + j])):
+                    num_errors += 1
+                    print("Error: the " + str(j) + " fitting factor of the line (from the photo_processes files (one each) for the original optimization and the generalization) is not the same. Below are the two lines that are not equal, where the first line is from the original optimization and the second line is from the generalization: " + photo_processes_original_lines[i] + photo_processes_gen_rmsd_gen_lines[i])
+else:
+    num_errors += 1
+    print("Error: The photo_processes.dat files for the original optimization and the generalization have different amounts of lines.")
+
+photo_processes_gen_rmsd_gen.close()
+photo_processes_original.close()
+
+if(num_errors == 0):
+    print("Success! The generalized optimization produced the same results as the original optimization.")
