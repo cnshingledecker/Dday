6106ca7ac78e7fc62cdcac6eb8d05714732d6601
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2020-11-21 23:41:22 -0600

    Created file for exportable custom python functions (can be used by multiple files)

---
+++ exportable_custom_functions.py
@@ -0,0 +1,13 @@
+def is_int(val):
+    try:
+        int(val)
+        return True
+    except ValueError:
+        return False
+
+def is_float(val):
+    try:
+        float(val)
+        return True
+    except ValueError:
+        return False
