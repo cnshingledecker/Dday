80752f6f90780ddfbcd6dcd39ea08d62ab06f282
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2021-01-26 07:35:34 -0600

    Created file to test multiprocessing in relation to the baragiola_optimization (this is a modified copy of the rmsd_generalization script)

---
+++ aaa_test_multiprocessing.py
@@ -0,0 +1,112 @@
+#! /usr/bin/python
+
+import os
+import csv
+import numpy as np
+import multiprocessing as mp
+import time
+import itertools
+from exportable_custom_functions import find_nearest_index,is_float, is_int
+
+def my_function(param1, param2, param3):
+    result = param1*param2*param3
+    return (param1, param2, param3, result)
+
+def get_result(result):
+    global results2
+    results2.append(result)
+
+
+if __name__ == '__main__':
+    results = open("resultsFile_2", 'w')
+
+    reactions = []
+    all_vector_args = [] # Holds a series of arrays (one for each of the linspaces that is created for a reaction)
+    num_modified_fitting_factors = 0
+    fitting_factors = [] # Holds arrays containing the fitting_factors for the reactions (each element of the array is a list with the various fitting factors for a reaction)
+
+    with open('reaction_fitting_factor_linspace_args/reaction_fitting_factor_vector_arguments.csv', newline='') as vector_creation_args_csv:  # Read in the parameters from the csv file for the creation of the linspaces (for each fitting factor to be varied)
+        reader = csv.reader(vector_creation_args_csv, delimiter=',')      
+        for i in range(0, 3): # Skips the first 3 lines of the csv file (lines which are comments)
+            fields = next(reader)
+        for row in reader:  # Each row is a set of arguments to be used to create the linspace for fitting factors for a reaction
+            num_modified_fitting_factors += 1
+            args_for_single_vector = []  # A vector to hold the set of arguments to be used to create the linspace for the fitting factors for a reaction
+            for argument in row:
+                if is_float(argument):
+                    args_for_single_vector.append(float(argument)) # Adds the arguments to the previously created vector (2 lines above)
+                else:  # It is text (it is not an index specifying a delta value to choose; because it is text, it must be the reaction(s) for which the fitting factors are being varied using the linspace created using some of the values in this row)
+                    reactions.append(argument)
+            all_vector_args.append(args_for_single_vector) 
+
+    for vector_args in all_vector_args:
+        single_vector_fitting_factors = np.linspace(vector_args[0], vector_args[1], num=int(vector_args[2])) # Creates numpy linspace of the fitting factors (for the reaction) using arguments peeviously retrieved from the csv file
+        single_vector_fitting_factors = list(single_vector_fitting_factors) # Converts the numpy linspace to a list
+        fitting_factors.append(single_vector_fitting_factors) 
+
+    all_fitting_factor_combinations = itertools.product(*fitting_factors)    # Creates all possible combinations of fitting factor values from the previously created lists (created in the 'for vector args in all_vector_args' for loop)
+                                                                            #    Note that this creates an array of n arrays (where n is the product of the number of values for each list of fitting factors), and each of these 
+                                                                            #    n arrays has k elements (where k is the number of fitting factors (num_modified_fitting_factors))
+    results2 = []
+    ts = time.time()
+    print('CPU count:', mp.cpu_count())
+    pool = mp.Pool(mp.cpu_count())
+    for fitting_factor_combination in all_fitting_factor_combinations:  # fitting_factor_combinations[n] is a value from the (n + 1)th np.linspace created 
+        pool.apply_async(my_function, args=(fitting_factor_combination[0], fitting_factor_combination[1], fitting_factor_combination[2]), callback=get_result)
+ 
+    pool.close()
+    pool.join()
+    print('Time in parallel:', time.time() - ts)
+    for k in results2:
+        print(k)
+
+#     infile = open("parameter_inputs_template.dat",'r')
+#     outfile = open("photo_processes_2.dat",'w')
+#     for line in infile:
+#         line_as_list = line.split()  # Convert the DAT file line into a list
+#         # We need to find the first index in the line (as a list) where there is a fitting factor value (it is the first number besides the first element of the list, hence why i (below) is set to 1)
+#         index_first_float_value_in_line = 1  # The first entry in the line is a float, but we are looking for the first fitting factor (the 2nd float value in the line); starting at 1 therefore makes the implementation of the below while loop easier
+#         # The while loop below skips the first value because it is an integer and would thus be counted as a float. 
+#         while(index_first_float_value_in_line < len(line_as_list) and not(is_float(line_as_list[index_first_float_value_in_line]))):  # Increments the index value until the first float value (fitting factor value) is found
+#             index_first_float_value_in_line += 1
+#         possible_fitting_factor_index = line_as_list[len(line_as_list) - 1] # See below comment for the meaning of this variable
+#         if(is_int(possible_fitting_factor_index)): # If the last value of the line is an integer, which means it is one of the reactions for which we are modifying the fitting factors
+#             new_fitting_factor_val = fitting_factor_combination[int(possible_fitting_factor_index)] # Convert the possible fitting factor index to an int and get the correct fitting factor (where fitting_factor_index tells the computer which fitting factor to get)
+#             new_fitting_factor_val = np.format_float_scientific(new_fitting_factor_val, precision=2,unique=False)  # Convert the new fitting factor value into a string of a number in scientific notation rounded to 2 places after the decimal point
+#             line = line[0:106] + new_fitting_factor_val + line[114:len(line)] # In the line, replace the old fitting factor with the new value             
+#         outfile.write(line)
+#     infile.close()
+#     outfile.close()
+#     print("Running model...")
+#     os.system('./runGeneralization.sh') # Executes a version of run.sh that runs the monaco executable for this generalization 
+#     print("Finding RMSD...")  # RMSD is root-mean square deviation
+
+#     num_experimental_data_points = 0
+#     with open('experimental_data/experimental_o3.csv') as csv_file: # Experimental data
+#         deviations = []
+#         csv_reader = csv.reader(csv_file, delimiter=',')
+#         for row in csv_reader: # This assumes that row[0] is the time, row[1] is the y-value (I'm not sure what this is),
+#             csv_model_data = open('csv/total_ice_o3.csv')
+#             csv_model_data_reader = csv.reader(csv_model_data, delimiter=',')
+#             csv_model_data_list = list(csv_model_data_reader)
+#             closest_model_values = csv_model_data_list[find_nearest_index(row[0], 0, csv_model_data_list)]
+#             deviation = float(closest_model_values[1])*1e7 - float(row[1])
+            
+#             # the deviation of the model from the y-value is allowed to be up to 10% away from the y-value
+#             if 0.9 * float(row[1]) < deviation < 1.1 * float(row[1]): # 0.9 * float(row[1]) is the allowed_lower_deviation, 1.1 * float(row[1]) is the allowed_upper_deviation
+#                 deviation = 0
+#             deviations.append(deviation)
+#             num_experimental_data_points += 1
+#         sum = 0
+#         for value in deviations:
+#             sum += (value**2)
+#         rmsd = (sum / (num_experimental_data_points - 2))**0.5   # Formula for RMSD
+
+#         # Should I create a boolean to only write to the output string if there was data in the experimental data csv file?
+#         output_string = "" # Create a string to hold the rmsd along with the fitting factor value for each reaction set (the fitting factor values combination)
+#         for i in range(0, len(reactions)): 
+#             output_string = output_string + str(fitting_factor_combination[i]) + "".join(" "*(23 - len(str(fitting_factor_combination[i])))) + reactions[i] + " delta values \n"
+#         output_string += str(rmsd) + "".join(" "*(23 - len(str(rmsd)))) + "RMSD" + "\n\n"
+#         results.write(output_string)
+# results.close()
+# print("Done!")
