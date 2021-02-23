71354dae15e587c31fd3b84f3bd5edd1440dfd16
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2021-02-22 22:35:53 -0600

    Removed 1 print statement (of matrix dimension) and modified the output string for each matrix size

--- TestMPI/test_random_arrays.py
+++ TestMPI/test_random_arrays.py
@@ -5,7 +5,6 @@ array_num_rows_and_num_cols = [32,64,128,256,512,1024,2048]
 times_ms = [0,0,0,0,0,0,0]
 for i in range(0,len(array_num_rows_and_num_cols)):
     length = array_num_rows_and_num_cols[i]
-    print(length)
     for j in range(0, 100):
         ts = time.time()
         array = np.random.rand(length, length)
@@ -14,4 +13,4 @@ for i in range(0,len(array_num_rows_and_num_cols)):
 
 print(times_ms)
 for i in range(0, len(times_ms)):
-    print(str(array_num_rows_and_num_cols[i]) + ": " + str(float(times_ms[i]/100)) + " For an average of " + str(times_ms[i]/(100*array_num_rows_and_num_cols[i])))
+    print("Matrix size: " + str(array_num_rows_and_num_cols[i]) +" x " + str(array_num_rows_and_num_cols[i]) + " "*(25-len("Matrix size: " + str(array_num_rows_and_num_cols[i]) +" x " + str(array_num_rows_and_num_cols[i]))) + " It took " + str(float(times_ms[i]/100)) + " "*(20-len(str(float(times_ms[i]/100)))) + " ms to make 100 matrices of this size, for an average time of " + str(times_ms[i]/(100*array_num_rows_and_num_cols[i])) + " "*(22-len(str(times_ms[i]/(100*array_num_rows_and_num_cols[i])))) +  " ms per matrix.")
