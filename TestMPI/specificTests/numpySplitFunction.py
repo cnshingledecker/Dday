9e8d28df21bf14dd54f3a9cd1adbf3530ece5289
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2021-02-22 23:41:40 -0600

    Added comments

--- TestMPI/specificTests/numpySplitFunction.py
+++ TestMPI/specificTests/numpySplitFunction.py
@@ -12,13 +12,13 @@ ts1 = time.time() * 1000
 data = [a,b,c]
 data = itertools.product(*data)
 l = [list(j) for j in np.array_split([list(j) for j in data],6)]
-time.sleep(0.5)
+time.sleep(0.5) # Added because the total runtime without it is a small number of milliseconds
 te1 = time.time() * 1000
 print("Way 1 (itertools.product(*data)) took " + str(te1 - ts1) + " milliseconds.")
 
 ts2 = (time.time() * 1000)
 data = itertools.product(*[a,b,c]) # This way takes longer as the number of arrays in data increases, but is comparable in terms of time for smaller test sets
 l = [list(j) for j in np.array_split([list(j) for j in data],6)]
-time.sleep(0.5)
+time.sleep(0.5) # Added because the total runtime without it is a small number of milliseconds
 te2 = (time.time() * 1000)
 print("Way 2 (itertools.product(*[a,b,c])) took " + str(te2 - ts2) + " milliseconds.")
