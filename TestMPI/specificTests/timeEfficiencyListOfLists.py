f577ecc6fb6a81eb03c6ff261278ff22cad91bbc
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2021-02-23 08:16:56 -0600

    Added comments and modified print statements

--- TestMPI/specificTests/timeEfficiencyListOfLists.py
+++ TestMPI/specificTests/timeEfficiencyListOfLists.py
@@ -1,6 +1,7 @@
+# This file tests: The amount of time it takes to generate a list of 3 numpy.linspaces using 2 different methods
+
 import numpy as np
 import time
-
 
 # No more than 10 to 12 milliseconds' difference between the two ways, 
 #     except when one the lists (not the ones with 10 and 1 element, respectively) is longer than 100 thousand elements; this increases the time
@@ -13,10 +14,10 @@ c = list(np.linspace(0.3,0.35,1000000))
 j = [a,b,c]
 time.sleep(0.5)
 te = time.time() * 1000
-print("Way 1 took " + str(te-ts) + " milliseconds.")
+print("Way 1 (creating the numpy linspaces and then creating a list of them) took " + str(te-ts) + " milliseconds.")
 
 ts = time.time() * 1000
 j = [list(np.linspace(0.3,0.35,1)), list(np.linspace(1,2,1)), list(np.linspace(1.7,2.7,10))]
 time.sleep(0.5)
 te = time.time() * 1000
-print("Way 2 took " + str(te-ts) + " milliseconds.")
+print("Way 2 (creating the numpy linspaces inside the creation of the list) took " + str(te-ts) + " milliseconds.")
