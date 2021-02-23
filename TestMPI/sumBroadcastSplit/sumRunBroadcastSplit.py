8bdca8f949e789918909d9202da5e3a7fb8ffe5e
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2021-02-23 08:54:00 -0600

    Added comments and modified a print statatement in sumRunBroadcastSplit.py

--- TestMPI/sumBroadcastSplit/sumRunBroadcastSplit.py
+++ TestMPI/sumBroadcastSplit/sumRunBroadcastSplit.py
@@ -1,3 +1,6 @@
+# This file does: For 1-4 processors, it runs nonParallelSumBroadcastSplit and sumBroadcastSplit (the parallel version),
+#                 having them each sum up certain numbers determined by the number of elements in linspaces (which come from the sum_linspace_vars.csv file)
+
 import csv, os, time
 import numpy as np
 from nonParallelSumBroadcastSplit import sum_num
@@ -5,27 +8,27 @@ from nonParallelSumBroadcastSplit import sum_num
 numpy_vars = []
 with open('sum_linspace_vars.csv', 'r') as file2:
     reader = csv.reader(file2, delimiter=",")
-    for line in reader:
+    for line in reader: # Add all number of elements in the numpy linspaces to a list
         numpy_vars.append([int(line[0]), int(line[1]), int(line[2])])
     file2.close()
 
-for j in range(1,5):
+for j in range(1,5): # J is the number of processors used in the parallel version
     sums_non_parallel = []
     sums_parallel = []
 
-    with open('sum_num_processors.csv', 'w') as num_processors_file:
+    with open('sum_num_processors.csv', 'w') as num_processors_file: # Writes the number of processors to be used by and in running the parallel sumBroadcastSplit script
         num_processors_file.write(str(j))
         num_processors_file.close()
 
-    for numpy_var_set in numpy_vars:
-        with open('sum_iter_vars.csv', 'w') as write_file:
+    for numpy_var_set in numpy_vars: 
+        with open('sum_iter_vars.csv', 'w') as write_file: # Write numpy linspace parameters to a file that the parallel sumBroadcastSplit script reads from and uses
             write_file.write(str(numpy_var_set[0]) + "," + str(numpy_var_set[1]) + "," + str(numpy_var_set[2]))
             write_file.close()
-        sums_non_parallel.append(sum_num(numpy_var_set[0], numpy_var_set[1], numpy_var_set[2]))
+        sums_non_parallel.append(sum_num(numpy_var_set[0], numpy_var_set[1], numpy_var_set[2])) # Find sum with non-parallel method and add it to a list
         os.system("mpiexec -n " + str(j) + " python3 sumBroadcastSplit.py")
-        with open('sum_parallel.csv', 'r') as file2:
+        with open('sum_parallel.csv', 'r') as file2: # Read in sum result (from parallel version) and add it to a list
             reader = csv.reader(file2, delimiter=",")
-            for line in reader:
+            for line in reader: # There is only 1 line in the file
                 sums_parallel.append((line[0]))
             file2.close()
 
@@ -35,5 +38,4 @@ for j in range(1,5):
         print("An error occurred with the parallel sum calculator: \n")
 
     for i in range(0, len(sums_non_parallel)):
-        # print("J: " + str(j) + " I: " + str(i))
-        print("For the np.linspace parameters " + str(numpy_vars[i]) + " "*(44-len("For the np.linspace parameters " + str(numpy_vars[i]))) + ", the difference between the parallel and non-parallel sums was " + str(np.float64(sums_parallel[i])) + " "*(18-len((str(np.float64(sums_parallel[i]))))) + " - " + str(sums_non_parallel[i]) + " "*(20-len(str(sums_non_parallel[i]))) +   " = " + str(np.float64(sums_parallel[i]) - sums_non_parallel[i]))
+        print("For the np.linspace parameters " + str(numpy_vars[i]) + " "*(48-len("For the np.linspace parameters " + str(numpy_vars[i]))) + ", the difference between the parallel and non-parallel sums was " + str(np.float64(sums_parallel[i])) + " "*(18-len((str(np.float64(sums_parallel[i]))))) + " - " + str(sums_non_parallel[i]) + " "*(20-len(str(sums_non_parallel[i]))) +   " = " + str(np.float64(sums_parallel[i]) - sums_non_parallel[i]))
