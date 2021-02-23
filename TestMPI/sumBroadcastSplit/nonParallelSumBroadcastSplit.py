d5fcde6a25e2e55ff7cd0e578539a582c92990ba
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2021-02-23 08:40:20 -0600

    Added comments

--- TestMPI/sumBroadcastSplit/nonParallelSumBroadcastSplit.py
+++ TestMPI/sumBroadcastSplit/nonParallelSumBroadcastSplit.py
@@ -1,11 +1,13 @@
+# This file does: Creates 3 numpy.linspaces and their Cartesian product, adds the sum of each combination to a sum variable, and returns that sum variable
+
 import numpy as np, itertools
 
 def sum_num(a,b,c):
     sum_total = 0
-    data = [list(np.linspace(1.7,2.7,a)), list(np.linspace(1,2,b)), list(np.linspace(0.3,0.35,c))]
-    data = itertools.product(*data)
-    data = [list(fitting_factor_combination) for fitting_factor_combination in data]
-
+    data = [list(np.linspace(1.7,2.7,a)), list(np.linspace(1,2,b)), list(np.linspace(0.3,0.35,c))] # Creates the numpy linspaces to be usedin making the cartesian product (all possible fitting factor combinations)
+    data = itertools.product(*data) # Creates the cartesian product of all possible fitting factor combinations
+    data = [list(fitting_factor_combination) for fitting_factor_combination in data] # Converts the result of itertools.product into a list (completely array-indexible)
+    
     for k in data:
         sum_total += sum(k)
 
