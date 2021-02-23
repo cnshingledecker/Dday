cf2ac8c7c66a637d5cdeacfbe75ac6fdaf79ae19
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2021-02-23 09:38:20 -0600

    Changed the name of the variable holding the output strings (and the variable name of the output string in the enhanced for loop where they are printed) and added comments

--- TestMPI/broadcastSplit/newRunBroadcastSplit.py
+++ TestMPI/broadcastSplit/newRunBroadcastSplit.py
@@ -1,15 +1,17 @@
+# This file tests: The time taken to create 100 matrices of various sizes (using parallel and non-parallel scripts)
+
 import csv, os, time
 
 matrix_dimensions = []
-with open('num_matrices.csv', 'r') as file2:
+with open('num_matrices.csv', 'r') as file2: # Get all the dimensions (N) to be used in the creation of NxN matrices in the two python scripts that are run below
     reader = csv.reader(file2, delimiter=",")
     matrix_dimensions = [int(line[0]) for line in reader]
     file2.close()
 
-j = []
+output_strings = []
 
 for matrix_dimension in matrix_dimensions:
-    with open('num_matrices_iteration.csv', 'w') as write_file:
+    with open('num_matrices_iteration.csv', 'w') as write_file: # Writes the matrix dimension that the non-parallel and parallel versions use (they read it from this file)
         write_file.write(str(matrix_dimension))
         write_file.close()
 
@@ -22,5 +24,5 @@ for matrix_dimension in matrix_dimensions:
     te2 = time.time() * 1000
     j.append("Matrix dim: " + str(matrix_dimension) + " "*(6-len(str(matrix_dimension))) + " Time: Non-par: " + (str(te2-ts2)) + " ms" + " "*(40-len(" Time: Non-par: " + (str(te2-ts2)) + " ms")) + " Par: " + str(te1-ts1) + " ms   Runtime ratio p/np: " + str((te1-ts1)/(te2-ts2)))
 
-for s in j:
-    print(s)
+for output_string in output_strings:
+    print(output_string)
