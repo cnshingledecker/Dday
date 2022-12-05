54c6843308f1476f700ed0e2d91334b9e7ed5d19
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2022-12-05 17:52:29 -0600

    Setup test cases for server running and modified plotting.py (not plotting without ion results because of an error with an unknown cause)

--- plotting.py
+++ plotting.py
@@ -1,6 +1,7 @@
 # Run DataFrameCreation.py before this
 
 import csv
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,7 +27,6 @@ custom_cycler = (cycler(color=['#098ec3', #blue
                 )
 
 # Change style defaults
-# BELOW TWO LINES--email Shingledecker about
 # plt.rc('text', usetex=True) # Allows LaTeX formatting with r'Formatted Text'
 # plt.rc('font',**{'family':'serif','serif':['Computer Modern Roman']}) # Changes font to default LaTeX fount
 plt.rcParams['axes.prop_cycle'] = custom_cycler # Sets plot color cycler to the cycle defined above
@@ -47,12 +47,13 @@ with open(w_ions_version, newline='') as bO3_csv:  # Read in the lines from the 
          while(True):
             bO3_csv_writer.writerow(next(reader))
       except StopIteration:
-         pass # We are done processing lines; this is the exception (expected) which is thrown when we are out of lines to write to the file.
-      
+         pass # We are done processing lines; this is the exception (expected) which is thrown when we are out of lines to write to the file. 
 
 # Load in data
 woions_df = pd.read_pickle(wo_ions_version + '/pickle_dataframes/csv_dataframe.pkl')
 wions_df = pd.read_csv(w_ions_modified_version)
+
+os.remove("./bO3_for_dataframe.csv") # Because we no longer need the file
 
 initialO2 = 5.7E22
 flux = 2.33e14
@@ -60,8 +61,6 @@ flux = 2.33e14
 # Isolate data of interest
 model_data_woions = woions_df[['Fluence', 'total_O3', 'total_O2']]
 model_data_wions = wions_df[['Fluence', 'bO3']]
-
-print(model_data_wions)
 
 exp_data = pd.DataFrame({'expX': [4.709604,
                                   13.496563,
@@ -77,7 +76,6 @@ exp_data = pd.DataFrame({'expX': [4.709604,
                                       1.6182984]})
 
 
-
 # Scale modifications
 exp_data["expY"] = exp_data["expY"] * (24 / 1.6182984) # Scaling factor used in figure_2_DO_NOT_MODIFY.pro.
 exp_data["expX"] = exp_data["expX"] * flux # (Old comment: Scaling factor used in figure_2_DO_NOT_MODIFY.pro.) Scaling factor now is different (flux value, which is different) 
@@ -86,14 +84,13 @@ exp_data["expX"] = exp_data["expX"] * flux # (Old comment: Scaling factor used i
 model_data_woions["Fluence"] = model_data_woions["Fluence"]
 
 model_data_wions["Fluence"] = model_data_wions["Fluence"]
-model_data_wions["bO3"] = (model_data_wions["bO3"] / initialO2) * 100
+model_data_wions["bO3"] = (model_data_wions["bO3"] / initialO2) * 100 * 1e3
 
-#model_data.head()
 # Create figure
 
 # Add data to figure
 ax = model_data_wions.plot(x = 'Fluence', y = 'bO3', label = 'Model with ions')
-model_data_woions.plot(ax = ax, x = 'Fluence', y = 'bO3', label = 'Without ions: Mullikin et al.')
+# model_data_woions.plot(ax = ax, x = 'Fluence', y = 'bO3', label = 'Without ions: Mullikin et al.')
 exp_data.plot.scatter(ax = ax, x = 'expX', y = 'expY', marker="x", c='dimgrey', label = 'Experiment: Gerakines et al.') #  c='#53b234'
 
 # Add plot elements
