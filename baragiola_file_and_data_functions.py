d48a7d7041672e2f41d4da908516ded08635df58
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2023-03-23 14:05:42 -0500

    Created functions for creating custom Bash scripts for checking a serial or parallel Baragiola script run's progress

--- baragiola_file_and_data_functions.py
+++ baragiola_file_and_data_functions.py
@@ -1,5 +1,80 @@
+import math, time
+
 def modelCSVFileName():
     return "./csv/bO3.csv"
+
+def serialProgressShellScriptGeneration(numFittingFactorCombinations):
+    numFittingFactorCombinationsDivided = math.floor(numFittingFactorCombinations / 100)
+    progressShellScriptString = f"""#!/usr/bin/bash
+
+resultsFilepathSerial=$(pwd)
+resultsFilepathFormatted="$resultsFilepathSerial/{serialAllOutputFileName()}"
+
+totalNumLines=`wc --lines < "$resultsFilepathFormatted"`
+
+linesPerSimulation=8
+approxNumSimulationsDone=$(expr $totalNumLines / $linesPerSimulation)
+
+# The operand on the left is the number of simulations being run
+approxNumSimulationsLeft=$(expr {numFittingFactorCombinations} - $approxNumSimulationsDone)
+
+# The operand on the right is for the number of simulations being run divided by 100, if it is not an integer, the nearest integer to it
+percentOfSimulationsDone=$(expr $approxNumSimulationsDone / {numFittingFactorCombinationsDivided})
+
+echo "Approximate Number of Simulations Done: $approxNumSimulationsDone of {numFittingFactorCombinations}."
+echo "Percent of Simulations Done: $percentOfSimulationsDone." 
+
+startTime={int(time.time())}
+
+echo "Start Time: " 
+TZ=America/Chicago date -d @$startTime +"%Y-%m-%d %I:%M %P" """
+    
+    with open("baragiolaProgress.sh", "w") as progressShellScriptFile:
+        progressShellScriptFile.write(progressShellScriptString)
+
+# original is a boolean parameter--supply True for the original parallel script, and False for the Model Nu Trial Nu Same parallel script
+def parallelProgressShellScriptGeneration(numFittingFactorCombinations, original):  
+    numFittingFactorCombinationsDivided = math.floor(numFittingFactorCombinations / 100)
+    parallelScriptOutputFileName = parallelAllOutputForCoreFileName() if original == True else parallelTrialNuIonNuSameAllOutputForCoreFileName()
+
+    progressShellScriptString = f"""#!/usr/bin/bash
+
+resultsFilepathParallel=$(pwd)
+
+core0filepath="$resultsFilepathParallel/baragiola_files_core0/{parallelScriptOutputFileName}"
+core1filepath="$resultsFilepathParallel/baragiola_files_core1/{parallelScriptOutputFileName}"
+core2filepath="$resultsFilepathParallel/baragiola_files_core2/{parallelScriptOutputFileName}"
+core3filepath="$resultsFilepathParallel/baragiola_files_core3/{parallelScriptOutputFileName}"
+
+core0numLines=`wc --lines < "$core0filepath"`
+core1numLines=`wc --lines < "$core1filepath"`
+core2numLines=`wc --lines < "$core2filepath"`
+core3numLines=`wc --lines < "$core3filepath"`
+
+resultsFilepath=$(pwd)
+resultsFilepathFormatted="$resultsFilepath/{serialAllOutputFileName()}"
+
+totalNumLines=`wc --lines < "$resultsFilepathFormatted"`
+
+linesPerSimulation=8
+approxNumSimulationsDone=$(expr $totalNumLines / $linesPerSimulation)
+
+# The operand on the left is the number of simulations being run
+approxNumSimulationsLeft=$(expr {numFittingFactorCombinations} - $approxNumSimulationsDone)
+
+# The operand on the right is for the number of simulations being run divided by 100, if it is not an integer, the nearest integer to it
+percentOfSimulationsDone=$(expr $approxNumSimulationsDone / {numFittingFactorCombinationsDivided})
+
+echo "Approximate Number of Simulations Done: $approxNumSimulationsDone of {numFittingFactorCombinations}."
+echo "Percent of Simulations Done: $percentOfSimulationsDone." 
+
+startTime={int(time.time())}
+
+echo "Start Time: " 
+TZ=America/Chicago date -d @$startTime +"%Y-%m-%d %I:%M %P" """
+    
+    with open("baragiolaProgress.sh", "w") as progressShellScriptFile:
+        progressShellScriptFile.write(progressShellScriptString)
 
 # For the parallel scripts
 def num_processors_to_use():
@@ -29,3 +104,6 @@ def parallelTrialNuIonNuSameBestResultsFileName():
 def process_model_data(data):
     initialO2 = 5.7E22
     return (data / initialO2) * 100
+
+def projectTimeZone():
+    return "America/Chicago"
